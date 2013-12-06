329053cd0230604bd5e421fbf3f315de97f66555
Root <root@getpantheon.com>
2013-12-06 16:44:05 +0000

    Import of site: default

---
+++ README.md
@@ -0,0 +1,4 @@
+fvc
+===
+
+fvc.local
---
+++ sites/all/themes/contrib/responsive_green/js/slide.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Responsive Green Slider Javascript.
+ *
+ */
+(function ($) {
+Drupal.behaviors.slider = {
+attach: function (context, settings) {
+  $(window).load(function() {
+    $("#single-post-slider").flexslider({
+      animation: 'slide',
+      slideshow: true,
+      controlNav: true,
+      smoothHeight: true,
+      slideshowSpeed: 7000,
+      direction: 'horizontal',
+      start: function(slider) {
+        slider.container.click(function(e) {
+          if(!slider.animating) {
+            slider.flexAnimate(slider.getTarget('next'));
+          }
+        });
+      }
+    });
+  });
+}
+};
+})(jQuery);
